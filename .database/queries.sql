9c3f9a7e6374bea25c55ddad5ab44059af202c6d
Tommy-Platt <tommy.platt@education.nsw.gov.au>
2024-11-21 12:56:38 +1100

    Folder Structure & SQL Tables
    Created the folder structure for the project and two SQL tables for Users and Guesses.

---
+++ .database/queries.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE Guesses (id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                       user_id INTEGER,
+--                       date TEXT NOT NULL,
+--                       score INTEGER,
+--                       game TEXT NOT NULL);
+
+-- CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     username TEXT UNIQUE NOT NULL,
+--                     password NOT NULL);
