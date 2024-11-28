7185e9cc4e34d3b484f6d2984a2446c51cefe37f
Tommy-Platt <tommy.platt@education.nsw.gov.au>
2024-11-28 11:35:01 +1100

    Setup Login & Logout Webpages
    Users can now login if their details match those of the users in the 'Users' database. They can also logout and are taken to the homepage.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+
 import db
 
 app = Flask(__name__)
@@ -8,5 +9,30 @@ app.secret_key = "gtg"
 def Home():
     guessData = db.GetAllGuesses() # Note: the new line
     return render_template("index.html", guesses=guessData) #Note: passing data
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    # They sent us data, get the username and password
+    # then check if their details are correct.
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session['id'] = user['id']
+            session['username'] = username
+
+            # Send them back to the homepage
+            return redirect("/")
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
 
 app.run(debug=True, port=5000)
